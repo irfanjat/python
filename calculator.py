b3f0e8f8d1eae170f55a191c3c3b8af142131d32
Irfan ALi <jatirfan110@gmail.com>
2026-02-21 21:32:40 +0500

    Basic calculator program

---
+++ calculator.py
@@ -0,0 +1,15 @@
+#Simple calculater program
+
+a = 10
+b = 20
+sum = a + b
+print ("The sum of two number is :",sum) 
+
+sub = a - b 
+print ("The subtraction is :",sub)
+
+mul = a * b
+print ("The multiplication of two numbers is:",mul)
+
+div = a /b 
+print ("The division of two number is:",div)
